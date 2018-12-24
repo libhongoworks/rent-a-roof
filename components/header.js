22bdc1a0f13976d3ab2073d75d7b53c08c30a8af
Libhongo Langa <lnglib001@myuct.ac.za>
2018-12-24 11:09:17 +0200

    incapsulated navbar-brand in nextjs link tags

--- components/header.js
+++ components/header.js
@@ -3,14 +3,16 @@ import Link from "next/link";
 const Header = () => (
   <div>
     <nav className="navbar container">
-      <a className="navbar-brand">
-        <img
-          src="../static/images/00108-real-estate-logos-design-free-house-logo-maker-01.png"
-          alt=""
-          height="72"
-        />
-        <span>Rent-A-Roof</span>
-      </a>
+      <Link href="">
+        <a className="navbar-brand">
+          <img
+            src="../static/images/00108-real-estate-logos-design-free-house-logo-maker-01.png"
+            alt=""
+            height="72"
+          />
+          <span>Rent-A-Roof</span>
+        </a>
+      </Link>
       <div className="userSignIn">
         <i className="far fa-user" />
         <a className="btn">
